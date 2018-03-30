489d17062eee80374bf2b2c12026a9a5aa2ddc51
Mateusz <mateuszjanusz95@gmail.com>
2018-03-30 22:51:23 +0100

    charts data

--- components/LineChart.js
+++ components/LineChart.js
@@ -6,8 +6,7 @@ import colors from '../colors'
 
 class Chart extends React.Component {
     render() {
-        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
-        // const data = this.props.data
+        const data = this.props.readings
         const contentInset = { top: 20, bottom: 20 }
 
         return (
--- screens/ChartScreen.js
+++ screens/ChartScreen.js
@@ -4,10 +4,52 @@ import {
     StyleSheet,
     Text,
     View,
-    Picker
+    Picker,
+    ActivityIndicator
 } from 'react-native';
 
+import colors from '../colors'
 import LineChart from '../components/LineChart';
+
+const {height, width} = Dimensions.get('window')
+
+async function getTodayReadings() {
+    try {
+        let response = await fetch(
+          'https://indoor-air-quality.herokuapp.com/today'
+        )
+        let response_json = await response.json();
+        return response_json
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getLastWeekReadings() {
+    try {
+        let response = await fetch(
+          'https://indoor-air-quality.herokuapp.com/last-week'
+        )
+        let response_json = await response.json();
+        return response_json
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getLastMonthReadings() {
+    try {
+        let response = await fetch(
+          'https://indoor-air-quality.herokuapp.com/last-month'
+        )
+        let response_json = await response.json();
+        return response_json
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
 
 
 export default class ChartScreen extends Component {
@@ -20,23 +62,52 @@ export default class ChartScreen extends Component {
         };
     }
 
+    async updateReadings(time_frame, top_chart_type, bottom_chart_type){
+        let readings
+        console.log(time_frame)
+        if(time_frame === 1){
+            readings = await getTodayReadings()
+        } else if(time_frame === 7){
+            readings = await getLastWeekReadings()
+        } else if(time_frame === 30){
+            readings = await getLastMonthReadings()
+        }
+
+        const top_chart_readings = readings.map((day) => day[top_chart_type])
+        const bottom_chart_readings = readings.map((day) => day[bottom_chart_type])
+
+        this.setState({
+            readings,
+            top_chart_readings,
+            bottom_chart_readings,
+        })
+    }
+
     async componentDidMount(){
-        
+        await this.updateReadings(this.state.time_frame, this.state.top_chart_type, this.state.bottom_chart_type)
     }
 
     render() {
+        if(!this.state.readings){
+            return (
+                <View style={[styles.container, {justifyContent: 'center'}]}>
+                    <ActivityIndicator size="large" color={colors.text} />
+                </View>
+            )
+        }
+
         return (
             <View style={styles.container}>
-                <View>
-                    <Picker style={styles.picker}
-                        selectedValue={this.state.time_frame}
-                        onValueChange={(itemValue, itemIndex) => this.setState({time_frame: itemValue})}
-                        >
-                            <Picker.Item label="Today" value="1" />
-                            <Picker.Item label="Last 7 days" value="7" />
-                            <Picker.Item label="Last 30 days" value="30" />
-                    </Picker>
-                </View>
+
+                <Picker style={styles.picker}
+                    selectedValue={this.state.time_frame}
+                    onValueChange={(itemValue, itemIndex) => this.setState({time_frame: itemValue})}
+                    >
+                        <Picker.Item label="Today" value={1} />
+                        <Picker.Item label="Last 7 days" value={7} />
+                        <Picker.Item label="Last 30 days" value={30} />
+                </Picker>
+
                 <Picker style={styles.picker}
                     selectedValue={this.state.top_chart_type}
                     onValueChange={(itemValue, itemIndex) => this.setState({top_chart_type: itemValue})}
@@ -45,7 +116,7 @@ export default class ChartScreen extends Component {
                         <Picker.Item label="Humidity" value="humidity" />
                         <Picker.Item label="Dust Density" value="dust" />
                 </Picker>
-                <LineChart type={this.state.top_chart_type} />
+                <LineChart type={this.state.top_chart_type} readings={this.state.top_chart_readings} />
                 <Picker style={styles.picker}
                     selectedValue={this.state.bottom_chart_type}
                     onValueChange={(itemValue, itemIndex) => this.setState({bottom_chart_type: itemValue})}
@@ -54,22 +125,32 @@ export default class ChartScreen extends Component {
                         <Picker.Item label="Humidity" value="humidity" />
                         <Picker.Item label="Dust Density" value="dust" />
                 </Picker>
-                <LineChart type={this.state.bottom_chart_type} />
+                <LineChart type={this.state.bottom_chart_type} readings={this.state.bottom_chart_readings} />
 
             </View>
         )
     }
+
+    async componentWillUpdate(new_props, new_state){
+        // console.log(new_state)
+        if( new_state.time_frame !== this.state.time_frame
+            || new_state.top_chart_type !== this.state.top_chart_type
+            || new_state.bottom_chart_type !== this.state.bottom_chart_type
+        ){
+            await this.updateReadings(new_state.time_frame, new_state.top_chart_type, new_state.bottom_chart_type)
+        }
+    }
+
 }
 
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 20,
-        padding: 8,
+        padding: 15,
         justifyContent: 'flex-start',
         // alignItems: 'center',
-        backgroundColor: '#2C2E38',
+        backgroundColor: colors.background,
     },
     row: {
         flex: 1,
@@ -85,15 +166,7 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
     },
     picker: {
-        color: 'white'
+        color: 'white',
+        width: width/2,
     },
-    green: {
-        color: '#4F9B51'
-    },
-    yellow: {
-        color: '#ECB02F'
-    },
-    red: {
-        color: '#C03221'
-    }
 });
